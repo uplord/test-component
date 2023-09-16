09fb7c08449793d651b65d8d6cc7258ec4025323
Michael <michael@uplord.co.uk>
2023-09-16 16:24:28 +0100

    First Commit

---
+++ index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Test</h1>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    methods: {
+
+    },
+    mounted() {
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @import 'style';
+</style>
